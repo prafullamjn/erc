55b0360ff6db4cbff336a9759d8d122afa4a34c2
prafullamjn <prafulla.mjn@gmail.com>
2022-02-01 11:56:18 +0545

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+## Bibliar
+
+Buscar en la Biblia de forma divertida y fácil. 
+
+Search the Bible in a fun and easy way.
+
+Note: This project is a prototype at the moment. 
+
+## Technology ##
+
+* Backend
+	* PHP 5
+	* MySQL
+* Frontend
+	* JQuery
+	* HTML & CSS
+
+## DataBase ##
+
+En la carpeta SQL estan los archivos para cargar en la Base de Datos. Luego modifique la configuración en api.php para conectarse a la DataBase.
+
+In the SQL folder are the files to load the Database. Then modify the settings in api.php to connect with the DataBase.
+
+## API ##
+
+El modelo Vista / Controlador esta totalmente separado a traves de un sistema de API. El archivo api.php sirve los datos en formato jSON.
+
+The View / Controller model is  separated through an API system. The api.php file provide the data in JSON format.
+
+## View ##
+
+La vista es facilmente modificable. Usted puede usar el diseño o embeber el código en cualquier parte de su sitio.
+
+The view is easily to Reskyn. You can use the design you want or embed the code anywhere in your site.
+
+## Join Us ##
+
+Si usted desea involucrarse en proyectos relacionados con la Biblia, por favor contacteme.
+
+If you want to be involved in projects related to the Bible, please contact me.
+
+Gracias!
---
+++ inc/APIBibliar.php
@@ -0,0 +1,124 @@
+<?php
+class APIBibliar {
+
+  var $version = '';
+  var $book = '';
+  var $chapter = '';
+  var $verse = '';
+
+  var $query = '';
+
+  function getVersions(){
+    global $db;
+
+    $return = array();
+
+    $sql = "SELECT * FROM `bible_version` WHERE published = 1 ORDER BY language ASC;";
+
+    $result = mysqli_query($db, $sql ) or die( json_encode( array( 'error' => 'Error: ' . mysql_error() ) ) );
+
+    while( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) $return[$row['id']] = $row;
+
+    return json_encode( $return );
+  }
+
+  function getBooks(){
+    global $db;
+
+    $return = array();
+
+    $sql = "SELECT id, name FROM `bible_".strtolower( $this->version )."_books` WHERE 1;";
+
+    $result = mysqli_query($db, $sql ) or die( json_encode( array( 'error' => 'Error: ' . mysql_error() ) ) );
+
+    while( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) $return[$row['id']] = $row;
+
+    return json_encode( $return );
+  }
+
+  function getChapters(){
+    global $db;
+
+    $return = array();
+
+    if( $this->version == 'TIS' && $this->book < 40 ) $this->book = 40; // Greek version only have New Testament
+
+    $sql = "SELECT DISTINCT chapter FROM `bible_".strtolower( $this->version )."` WHERE book = '".$this->book."';";
+
+    $result = mysqli_query($db, $sql ) or die( json_encode( array( 'error' => 'Error: ' . mysql_error() ) ) );
+
+    while( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) $return[] = $row;
+
+    return json_encode( $return );
+  }
+
+  function getVersicle(){
+    global $db;
+
+    $return = array();
+
+    if( $this->version == 'TIS' && $this->book < 40 ){ // Greek version only have New Testament
+      $this->book = 40;
+      $this->chapter = 1;
+    }
+
+    $sql = "SELECT verse FROM `bible_".strtolower( $this->version )."` WHERE book = '".$this->book."' AND chapter = '".$this->chapter."';";
+
+    $result = mysqli_query($db, $sql ) or die( json_encode( array( 'error' => 'Error: ' . mysql_error() ) ) );
+
+    while( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) $return[] = $row;
+
+    return json_encode( $return );
+  }
+
+  function getBible(){
+    global $db;
+
+    $return = array();
+
+    if( in_array( $this->book, array( 'null', '', 'undefined' ) ) ) $this->book = '';
+    if( in_array( $this->chapter, array( 'null', '', 'undefined' ) ) ) $this->chapter = '';
+    if( in_array( $this->verse, array( 'null', '', 'undefined' ) ) ) $this->verse = '';
+
+    if( $this->version == 'TIS' && $this->book < 40 ){ // Greek version only have New Testament
+      $this->book = 40;
+      $this->chapter = 1;
+    }
+
+    $sql = "SELECT * FROM `bible_".strtolower( $this->version )."` WHERE book = '".$this->book."' "
+    ." AND chapter = '".$this->chapter."' "
+    .( ( !empty( $this->verse ) )? " AND verse = '".$this->verse."' " : '' )
+    .";";
+
+    $result = mysqli_query($db, $sql ) or die( json_encode( array( 'error' => 'Error: ' . mysql_error() ) ) );
+
+    while( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) $return[$row['book'].'-'.$row['chapter'].'-'.$row['verse']] = $row;
+
+    return json_encode( $return );
+  }
+
+  function getSearch(){
+    global $db;
+
+  	$return = array();
+
+    if( !empty( $this->query ) ){
+
+      if( in_array( $this->book, array( 'null', '', 'undefined' ) ) ) $this->book = '';
+      if( in_array( $this->chapter, array( 'null', '', 'undefined' ) ) ) $this->chapter = '';
+
+      $sql = "SELECT t.verse, t.chapter, t.book, t.text, b.name as bookname FROM `bible_".strtolower( $this->version )."` as t "
+      ." JOIN `bible_".strtolower( $this->version )."_books` as b ON( b.id = t.book ) "
+      ." WHERE t.text LIKE '% ".$this->query." %' "
+      .( ( !empty( $this->book ) )? " AND t.book = '".$this->book."' " : '' )
+      .( ( !empty( $this->chapter ) )? " AND t.chapter = '".$this->chapter."' " : '' )
+      .";";
+
+      $result = mysqli_query($db, $sql ) or die( json_encode( array( 'error' => 'Error: ' . mysql_error() ) ) );
+  
+      while( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) $return[$row['book'].'-'.$row['chapter'].'-'.$row['verse']] = $row;
+    }
+		return json_encode( $return );
+  }
+
+}
